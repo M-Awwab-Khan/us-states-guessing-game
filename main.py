0cb62b71e8501972ae3d84d0701961bc553cdb3d
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 19:47:01 +0500

    game end logic and left states csv

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 import turtle
 import pandas as pd
 
@@ -15,16 +15,22 @@ turtle.shape(IMAGE)
 writer = turtle.Turtle()
 writer.penup()
 writer.hideturtle()
-
-i = 1
+guessed_states = []
+i = 1   
 while i <= 50:
     answer_state = screen.textinput(title= f"Guess the State {i}/50", prompt="What's another stat's name? ")
-    user_guess = answer_state.title()
-    if user_guess in states['state'].values:
-        x = states[states['state'] == user_guess].x.item()
-        y = states[states['state'] == user_guess].y.item()
-        writer.goto(x, y)
-        writer.write(user_guess, align='left', font=('Arial', 7, 'normal'))
-        i += 1
+    if answer_state != None:
+        user_guess = answer_state.title()
+
+        if user_guess in states['state'].values:
+            guessed_states.append(user_guess)
+            x = states[states['state'] == user_guess].x.item()
+            y = states[states['state'] == user_guess].y.item()
+            writer.goto(x, y)
+            writer.write(user_guess, align='left', font=('Arial', 7, 'normal'))
+            i += 1
     else:
-        print('doesnt exists')
+        left_states = list(set(states['state']).difference(set(guessed_states)))
+        left_states.sort()
+        pd.DataFrame({'states': left_states}).to_csv('states_to_learn.csv')
+        break
