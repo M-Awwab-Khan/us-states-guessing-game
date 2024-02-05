b0de5d91d5ed7933bad1b665aa50ef8bf61eba8c
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 19:25:29 +0500

    improved the game logic and syntax

--- main.py
+++ main.py
@@ -11,23 +11,20 @@ screen.title('U.S. States Game')
 IMAGE = 'blank_states_img.gif'
 screen.addshape(IMAGE)
 turtle.shape(IMAGE)
-screen.tracer(0)
-turtle.penup()
+
+writer = turtle.Turtle()
+writer.penup()
+writer.hideturtle()
 
 i = 1
-while True:
+while i <= 50:
     answer_state = screen.textinput(title= f"Guess the State {i}/50", prompt="What's another stat's name? ")
     user_guess = answer_state.title()
     if user_guess in states['state'].values:
-        x = int(states[states['state'] == answer_state.title()]['x'])
-        y = int(states[states['state'] == answer_state.title()]['y'])
-        print(x, y)
-        turtle.goto(x, y)
-        turtle.write(user_guess, align='left', font=('Arial', 5, 'normal'))
-        turtle.goto(0, 0)
-        screen.update()
+        x = states[states['state'] == user_guess].x.item()
+        y = states[states['state'] == user_guess].y.item()
+        writer.goto(x, y)
+        writer.write(user_guess, align='left', font=('Arial', 7, 'normal'))
         i += 1
     else:
         print('doesnt exists')
-
-turtle.mainloop()
