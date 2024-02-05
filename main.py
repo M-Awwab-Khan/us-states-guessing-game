961b3aef35aa299d6279d1db05a2c0a40301c6e5
Awwab <mawwabkhank2006@gmail.com>
2024-02-05 18:46:41 +0500

    got states gif on the window

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+import turtle
+
+screen = Screen()
+screen.title('U.S. States Game')
+
+
+IMAGE = 'blank_states_img.gif'
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+
+
+turtle.mainloop()
